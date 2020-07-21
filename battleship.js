7c6b4414b15babaab60ed9a38faed6017fcc9417
ThreeT <7hr0771e@gmail.com>
2020-07-22 00:03:46 +0300

    Настройка логики. Запуск цикла, получение данных от пользователя и их проверка на корректность.

--- battleship.js
+++ battleship.js
@@ -6,3 +6,12 @@ let location1 = 3,
     hits = 0,
     isSunk = false;
 
+while (!isSunk) {
+    guess = prompt('Введите координаты корабля (от 0 до 6):');
+
+    if (guess < 0 || guess > 6) {
+        alert('Пожалуйста, введите корректный номер!');
+    } else {
+        guesses++;
+    }
+}
